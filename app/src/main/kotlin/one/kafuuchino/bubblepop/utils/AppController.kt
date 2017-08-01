e35f2a1413cc12f30c71c74e14a6259899824534
Junseok Oh <kotohana5706@edcan.kr>
2017-08-02 07:47:08 +0900

    Remove Facebook Initialization

--- app/src/main/kotlin/one/kafuuchino/bubblepop/utils/AppController.kt
+++ app/src/main/kotlin/one/kafuuchino/bubblepop/utils/AppController.kt
@@ -13,7 +13,6 @@ class AppController : Application() {
     override fun onCreate() {
         super.onCreate()
         context = applicationContext
-        FacebookSdk.sdkInitialize(this)
     }
 
     companion object {
