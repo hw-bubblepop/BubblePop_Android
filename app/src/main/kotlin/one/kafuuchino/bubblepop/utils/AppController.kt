19d422d8df1276e573e4f6405dabf76c86ebae24
Junseok Oh <kotohana5706@edcan.kr>
2017-08-02 06:55:57 +0900

    Initialize Android Project with Kotlin

---
+++ app/src/main/kotlin/one/kafuuchino/bubblepop/utils/AppController.kt
@@ -0,0 +1,13 @@
+package one.kafuuchino.bubblepop.utils
+
+import android.app.Application
+
+/**
+ * Created by Junseok Oh on 2017-07-09.
+ */
+
+class AppController : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
