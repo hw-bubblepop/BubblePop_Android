8f757444f4ef527d543202746998e00193d649ec
Junseok Oh <kotohana5706@edcan.kr>
2017-09-22 01:57:05 +0900

    [Activity] Add missing activities, fragment * MainStoreFragment * CardEditActivity * StoreItemViewActivity * StudyInfoViewActivity

---
+++ app/src/main/kotlin/us/buddman/bubblepop/CardEditActivity.kt
@@ -0,0 +1,12 @@
+package us.buddman.bubblepop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CardEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_edit)
+    }
+}
