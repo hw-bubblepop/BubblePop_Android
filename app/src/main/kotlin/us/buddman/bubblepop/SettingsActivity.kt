c1461b907e1f150020c59665117cf0bb39a19487
Junseok Oh <kotohana5706@edcan.kr>
2017-09-27 03:21:54 +0900

    [SettingsActivity] Build SettingsActivity Layout

--- app/src/main/kotlin/us/buddman/bubblepop/SettingsActivity.kt
+++ app/src/main/kotlin/us/buddman/bubblepop/SettingsActivity.kt
@@ -3,12 +3,29 @@ package us.buddman.bubblepop
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import com.github.nitrico.lastadapter.LastAdapter
+import kotlinx.android.synthetic.main.activity_settings.*
+import us.buddman.bubblepop.databinding.SettingsContentBinding
 
 class SettingsActivity : BaseActivity() {
 
-
+    var settingsArray : ArrayList<Pair<String, String>> = arrayListOf(
+            Pair("버블카드 편집", "내 버블카드를 편집합니다."),
+            Pair("프로필 편집", "프로필 사진, 정보 등을 편집합니다."),
+            Pair("스터디 및 모임 관리", "내가 생성한 스터디나 모임을 확인하고 관리합니다."),
+            Pair("스토어 보관함", "스토어에서 구매한 상품을 관리합니다."),
+            Pair("공지사항 및 이벤트", "BubblePop의 공지사항과 이벤트를 확인합니다.")
+    )
+    var adapter : LastAdapter? = null
     override fun setDefault() {
-        startActivity(Intent(applicationContext, MyProfileSettingsActivity::class.java))
+        settingsRecyclerView.layoutManager = LinearLayoutManager(applicationContext)
+        adapter = LastAdapter(settingsArray, BR.content)
+                .map<Pair<String, String>, SettingsContentBinding>(R.layout.settings_content){
+                    onBind {  }
+                }
+                .into(settingsRecyclerView)
+
     }
 
     override fun onCreateViewId(): Int {
