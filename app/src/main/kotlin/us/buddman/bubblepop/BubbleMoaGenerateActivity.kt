21c13af0241ed36fa53cd619aa6e6efa78ebf20b
Junseok Oh <kotohana5706@edcan.kr>
2017-09-26 13:14:15 +0900

    [StudyInfoViewActivity] Build StudyInfoViewActivity Layout

--- app/src/main/kotlin/us/buddman/bubblepop/BubbleMoaGenerateActivity.kt
+++ app/src/main/kotlin/us/buddman/bubblepop/BubbleMoaGenerateActivity.kt
@@ -1,19 +1,23 @@
 package us.buddman.bubblepop
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import com.github.nitrico.lastadapter.LastAdapter
+import kotlinx.android.synthetic.main.activity_study_moim_manage.*
+import us.buddman.bubblepop.databinding.StudyMoimManageContentBinding
+import us.buddman.bubblepop.models.Moim
+import us.buddman.bubblepop.models.Study
+import java.util.*
 
 class BubbleMoaGenerateActivity : BaseActivity() {
-
     override fun setDefault() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewToolbarId(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onCreateViewId(): Int {
         return R.layout.activity_bubble_moa_generate
     }
-
-    override fun onCreateViewToolbarId(): Int {
-        return 0
-    }
-
 }
