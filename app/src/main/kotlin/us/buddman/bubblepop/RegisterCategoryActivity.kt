5e7d9ed523f8c0952a7b73aa21c5157b32425325
grooshbene <wltn000129@gmail.com>
2017-09-27 23:03:23 +0900

    [RegisterCategoryActivity] Build RegisterCategoryActivity Layout

--- app/src/main/kotlin/us/buddman/bubblepop/RegisterCategoryActivity.kt
+++ app/src/main/kotlin/us/buddman/bubblepop/RegisterCategoryActivity.kt
@@ -1,10 +1,56 @@
 package us.buddman.bubblepop
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import android.graphics.Color
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.GridLayoutManager
+import com.github.nitrico.lastadapter.LastAdapter
+import kotlinx.android.synthetic.main.activity_register_category.*
+import us.buddman.bubblepop.databinding.ContentRegisterCategoryBinding
 
 class RegisterCategoryActivity : BaseActivity() {
+    var likeArray: ArrayList<LikeType> = arrayListOf(
+            LikeType("교육"),
+            LikeType("강연"),
+            LikeType("세미나/컨퍼런스"),
+            LikeType("문화/예술"),
+            LikeType("취미활동"),
+            LikeType("소모임/친목행사"),
+            LikeType("공모전"),
+            LikeType("전시/박람회"),
+            LikeType("패션/뷰티"),
+            LikeType("이벤트/파티"),
+            LikeType("여행"),
+            LikeType("후원금 모금"),
+            LikeType("등산"),
+            LikeType("봉사활동"),
+            LikeType("공연/전시"),
+            LikeType("음악/악기"),
+            LikeType("낚시"),
+            LikeType("차/오토바이"),
+            LikeType("자전거"),
+            LikeType("취미, 여가, 여행"),
+            LikeType("사교/인맥"),
+            LikeType("기타")
+    )
+    var layoutManager: GridLayoutManager? = null
+    var adapter: LastAdapter? = null
     override fun setDefault() {
+        layoutManager = GridLayoutManager(applicationContext, 2)
+        registerCategoryRecyclerView.layoutManager = layoutManager
+        adapter = LastAdapter(likeArray, BR.content)
+                .map<LikeType, ContentRegisterCategoryBinding>(R.layout.content_register_category) {
+                    onBind { holder ->
+                        run {
+                            holder.binding.registerContentTextView.setBackgroundResource(if (likeArray[holder.adapterPosition].isLike) R.drawable.round_blue_square else R.drawable.round_transparent_blue_square)
+                            holder.binding.registerContentTextView.setTextColor(if (likeArray[holder.adapterPosition].isLike) Color.WHITE else ContextCompat.getColor(applicationContext, R.color.colorPrimary))
+                        }
+                    }
+                    onClick {
+                        likeArray[it.adapterPosition].isLike = !likeArray[it.adapterPosition].isLike
+                        adapter!!.notifyDataSetChanged()
+                    }
+                }
+                .into(registerCategoryRecyclerView)
     }
 
     override fun onCreateViewId(): Int {
@@ -15,3 +61,5 @@ class RegisterCategoryActivity : BaseActivity() {
         return 0
     }
 }
+
+data class LikeType(var title: String, var isLike: Boolean = false)
