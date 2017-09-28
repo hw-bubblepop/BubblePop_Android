825b1d6e38d3929ae8172e9df22b3adfee695a3a
Junseok Oh <kotohana5706@edcan.kr>
2017-09-28 23:22:02 +0900

    [NetworkAPI] Update API Lists related to authentication

--- app/src/main/kotlin/us/buddman/bubblepop/utils/NetworkAPI.kt
+++ app/src/main/kotlin/us/buddman/bubblepop/utils/NetworkAPI.kt
@@ -1,11 +1,10 @@
 package us.buddman.bubblepop.utils
 
+import android.support.annotation.Nullable
+import okhttp3.ResponseBody
 import us.buddman.bubblepop.models.User
 import retrofit2.Call
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.*
 
 /**
  * Created by Junseok Oh on 2017-08-02.
@@ -14,6 +13,32 @@ interface NetworkAPI {
 
     @POST("/auth/login")
     @FormUrlEncoded
-    fun loginByLocal(email : String, password : String) : Call<User>
+    fun loginByLocal(
+            @Field("email") email: String,
+            @Field("password") password: String
+    ): Call<User>
+
+    @POST("/auth/register")
+    @FormUrlEncoded
+    fun registerByLocal(
+            @Field("email") email: String,
+            @Field("password") password: String,
+            @Field("nickname") nickname: String,
+            @Field("age") age: Int,
+            @Field("location") location: Int,
+            @Field("star") star: ArrayList<Int>): Call<User>
+
+    @POST("/user/update/nickname")
+    @FormUrlEncoded
+    fun updateNickname(
+            @Field("_id") _id: String,
+            @Field("nickname") nickname: String): Call<ResponseBody>
+
+    @POST("/user/update/password")
+    @FormUrlEncoded
+    fun updatePassword(
+            @Field("_id") _id: String,
+            @Field("password") password: String): Call<ResponseBody>
+
 
 }
